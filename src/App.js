b2b95ac0a7e3ba0db16d6417f65ffcb4a583f5a4
Anton Nocum <anton.nocum@gmail.com>
2019-05-12 22:00:26 +0800

    Fixing Heroku Build - Renaming views/Dashboard to views/Meh

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Layout } from 'antd'
 import Sidebar from './components/Sidebar'
-import Dashboard from './views/Dashboard'
+import Dashboard from './views/Meh'
 import TradeLogs from './views/TradeLogs'
 
 import 'antd/dist/antd.css'
